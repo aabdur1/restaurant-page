1b18f887a7fc1379a217331d82cfaef9fe63ee8a
Amir Abdur-Rahim <amirabdurrahim@gmail.com>
2024-03-30 14:14:04 -0500

    Fixed pics and added styling

---
+++ src/header.js
@@ -0,0 +1,33 @@
+import Logo from "./images/logo.png";
+
+function header() {
+  const header = document.createElement("header");
+  header.classList.add("header");
+  const headerTitle = document.createElement("h1");
+  headerTitle.classList.add("header-title");
+  headerTitle.textContent = "Pablo's Patisserie";
+  header.appendChild(headerTitle);
+  const logo = document.createElement("img");
+  logo.src = Logo;
+  logo.alt = "Logo";
+  logo.classList.add("logo");
+  header.appendChild(logo);
+  const nav = document.createElement("nav");
+  nav.classList.add("header-nav");
+  header.appendChild(nav);
+  const homeBtn = document.createElement("button");
+  homeBtn.classList.add("nav-btn");
+  homeBtn.textContent = "Home";
+  nav.appendChild(homeBtn);
+  const menuBtn = document.createElement("button");
+  menuBtn.classList.add("nav-btn");
+  menuBtn.textContent = "Menu";
+  nav.appendChild(menuBtn);
+  const aboutBtn = document.createElement("button");
+  aboutBtn.classList.add("nav-btn");
+  aboutBtn.textContent = "About us";
+  nav.appendChild(aboutBtn);
+  return header;
+}
+
+export default header;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("test");
+import "./style.css";
+import header from "./header.js";
+import PabloImg from "./images/pablo.png";
+
+document.body.appendChild(header());
+
+function mainSection() {
+  const section = document.createElement("section");
+  const imgWrapper = document.createElement("div");
+  imgWrapper.classList.add("img-wrapper");
+  section.appendChild(imgWrapper);
+  const pabloImg = new Image();
+  pabloImg.src = PabloImg;
+  pabloImg.alt = "Pablo licking his lips";
+  pabloImg.classList.add("pabloImg");
+  imgWrapper.appendChild(pabloImg);
+  const pabloImgLabel = document.createElement("label");
+  pabloImgLabel.for = pabloImg;
+  pabloImgLabel.classList.add("pablo-img-label");
+  pabloImgLabel.textContent = "Chef Pablo";
+  imgWrapper.appendChild(pabloImgLabel);
+  const mainWrapper = document.createElement("div");
+  mainWrapper.className = "main-wrapper";
+  section.appendChild(mainWrapper);
+  const title = document.createElement("h1");
+  title.textContent = "Pablo's Patisserie";
+  title.className = "title";
+  mainWrapper.appendChild(title);
+  const aboutSnippet = document.createElement("p");
+  aboutSnippet.textContent =
+    "Welcome to Pablo’s Patisserie, where our friendly orange tabby cat, Pablo, crafts delectable treats that blend Parisian elegance with a cozy, community vibe. Inspired by his love for fine pastries and equipped with a whisk, a bowl, and a dream, Pablo brings to life a menu of handcrafted delights—from buttery croissants to delicate macarons. At Pablo’s Patisserie, we’re more than just a bakery; we’re a gathering place for warmth, laughter, and the sweet aroma of baking. Join us for a slice of heaven and a touch of cat magic, where every bite tells a story of passion, tradition, and the joy of sharing.";
+  aboutSnippet.className = "about";
+  mainWrapper.appendChild(aboutSnippet);
+  return section;
+}
+
+document.body.appendChild(mainSection());
